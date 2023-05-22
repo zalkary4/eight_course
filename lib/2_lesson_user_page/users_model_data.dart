b5df787c882d3fb8b6053b9d964d7b89c3e8af63
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-23 01:06:30 +0600

    update

---
+++ lib/2_lesson_user_page/users_model_data.dart
@@ -0,0 +1,126 @@
+const String constText =
+    'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.';
+
+final List<User> users = [
+  User(
+    atyJonu: 'Aydar',
+    kesibi: 'Flutter developer',
+    jash: 20,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Adilbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Aydar',
+    kesibi: 'Flutter developer',
+    jash: 20,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Adilbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Aydar',
+    kesibi: 'Flutter developer',
+    jash: 20,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Adilbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Aydar',
+    kesibi: 'Flutter developer',
+    jash: 20,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Adilbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Aydar',
+    kesibi: 'Flutter developer',
+    jash: 20,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Adilbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Meerim',
+    kesibi: 'Flutter developer',
+    jash: 19,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Albina',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Mayrambek',
+    kesibi: 'Flutter developer',
+    jash: 43,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Altynbek',
+    kesibi: 'Flutter developer',
+    jash: 32,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Jamal',
+    kesibi: 'Flutter developer',
+    jash: 27,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+];
+
+class User {
+  final String atyJonu;
+  final String kesibi;
+  final int jash;
+  final String profileSurot;
+  final String tajryybaJolu;
+
+  User({
+    required this.atyJonu,
+    required this.kesibi,
+    required this.jash,
+    required this.profileSurot,
+    required this.tajryybaJolu,
+  });
+}
