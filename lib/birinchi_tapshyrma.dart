921ee5a7c85199ce69fd28826626a6e48a49c815
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-22 23:05:08 +0600

    update

--- lib/birinchi_tapshyrma.dart
+++ lib/birinchi_tapshyrma.dart
@@ -14,8 +14,16 @@ class _BirinchiTapshyrmaCounterPageState
     extends State<BirinchiTapshyrmaCounterPage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {});
+  void koshuu() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void kemituu() {
+    setState(() {
+      _counter--;
+    });
   }
 
   @override
@@ -32,12 +40,12 @@ class _BirinchiTapshyrmaCounterPageState
             const SizedBox(height: 90),
             const FlutterLogo(size: 150),
             const Text('This is the first task'),
-            const Card(
+            Card(
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 16, horizontal: 70),
                 child: Text(
-                  '0',
-                  style: TextStyle(fontSize: 30, color: Colors.red),
+                  '$_counter',
+                  style: const TextStyle(fontSize: 30, color: Colors.red),
                 ),
               ),
             ),
@@ -45,13 +53,13 @@ class _BirinchiTapshyrmaCounterPageState
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: koshuu,
                   child: const Icon(Icons.add),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: kemituu,
                   child: const Icon(Icons.remove),
-                )
+                ),
               ],
             )
           ],
