accab186f646fa6641e0714563384aa7bae2410f
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-23 00:47:17 +0600

    update

--- lib/2_lesson_user_page/user_page.dart
+++ lib/2_lesson_user_page/user_page.dart
@@ -1,6 +1,59 @@
 import 'package:flutter/material.dart';
 
-final List users = [];
+const String constText =
+    'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.';
+
+final List<User> users = [
+  User(
+    atyJonu: 'Aydar',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Adilbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Meerim',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Albina',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Mayrambek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Altynbek',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+  User(
+    atyJonu: 'Jamal',
+    kesibi: 'Flutter developer',
+    jash: 23,
+    profileSurot: 'FlutterLogo(size: 40)',
+    tajryybaJolu: constText,
+  ),
+];
 
 class User {
   final String atyJonu;
