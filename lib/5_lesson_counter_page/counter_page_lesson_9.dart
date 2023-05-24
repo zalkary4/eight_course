690317454bf53131dc46f6bb7e75917dfa886cb6
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-24 22:16:17 +0600

    update

--- lib/5_lesson_counter_page/counter_page_lesson_9.dart
+++ lib/5_lesson_counter_page/counter_page_lesson_9.dart
@@ -7,9 +7,32 @@ class CounterPageLesson9 extends StatefulWidget {
   _CounterPageLesson9State createState() => _CounterPageLesson9State();
 }
 
+int san = 0;
+
 class _CounterPageLesson9State extends State<CounterPageLesson9> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter 1')),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Text('$san', style: const TextStyle(fontSize: 70)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Icon(Icons.remove),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
