986caddc0622414201ce62d28be98711a2e87207
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-25 00:01:48 +0600

    update

--- lib/10_lesson_f7/login_page.dart
+++ lib/10_lesson_f7/login_page.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
+
+  String login = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login Page')),
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                onChanged: (val) {
+                  login = val;
+                  print(val);
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Login',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                onChanged: (log) {
+                  password = log;
+                  print(log);
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Input'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
