539d547a2b48bc2750ffbabd4ac127f075686bbc
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-26 00:16:34 +0600

    update

---
+++ lib/11_lesson_f8/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPageF8 extends StatelessWidget {
+  const LoginPageF8({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login Page')),
+    );
+  }
+}
