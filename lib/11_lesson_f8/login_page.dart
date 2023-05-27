464a5d44e3ff4a1d5527d88593bb6e8eb267d239
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-27 18:14:26 +0600

    update

--- lib/11_lesson_f8/login_page.dart
+++ lib/11_lesson_f8/login_page.dart
@@ -24,7 +24,7 @@ class LoginPageF8 extends StatelessWidget {
           SizedBox(height: 30),
           Text(
             'Yondu Udonta',
-            style: TextStyle(color: Colors.white, fontSize: 40),
+            style: TextStyle(color: Colors.white, fontSize: 30),
           ),
           TextField(),
         ],
