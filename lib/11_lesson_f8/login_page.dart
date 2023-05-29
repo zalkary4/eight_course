462487896325b164ee25f867012ddb08218093aa
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-05-29 23:08:05 +0600

    update

--- lib/11_lesson_f8/login_page.dart
+++ lib/11_lesson_f8/login_page.dart
@@ -14,99 +14,104 @@ class LoginPageF8 extends StatelessWidget {
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.w800)),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 30),
-          CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/images/yondu.jpg')),
-          SizedBox(height: 30),
-          Text(
-            'Yondu Udonta',
-            style: GoogleFonts.pacifico(
-              color: Colors.white,
-              fontSize: 30,
-            ),
-          ),
-          Text(
-            'Flutter DEVELOPER',
-            style: GoogleFonts.dosis(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 30),
+              CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/yondu.jpg')),
+              SizedBox(height: 30),
+              Text(
+                'Yondu Udonta',
+                style: GoogleFonts.pacifico(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                'Flutter DEVELOPER',
+                style: GoogleFonts.dosis(
+                    color: Color.fromARGB(255, 197, 212, 205),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400),
+              ),
+              const Divider(
+                thickness: 1,
                 color: Color.fromARGB(255, 197, 212, 205),
-                fontSize: 20,
-                fontWeight: FontWeight.w400),
-          ),
-          const Divider(
-            thickness: 1,
-            color: Color.fromARGB(255, 197, 212, 205),
-            indent: 185,
-            endIndent: 185,
-          ),
-          SizedBox(height: 10),
-          SizedBox(
-            height: 50,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextFormField(
-                initialValue: "+996 777 111 222",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 66, 98, 67),
-                  fontSize: 20,
-                ),
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.phone,
-                    color: Color.fromARGB(255, 66, 98, 67),
-                    size: 25,
+                indent: 185,
+                endIndent: 185,
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 50,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextFormField(
+                    initialValue: "+996 777 111 222",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 66, 98, 67),
+                      fontSize: 20,
+                    ),
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Color.fromARGB(255, 66, 98, 67),
+                        size: 25,
+                      ),
+                      isDense: true,
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
                   ),
-                  isDense: true,
-                  filled: true,
-                  fillColor: Colors.white,
                 ),
               ),
-            ),
-          ),
-          SizedBox(height: 10),
-          SizedBox(
-            height: 50,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextFormField(
-                initialValue: "example@gmail.com",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 66, 98, 67),
-                  fontSize: 20,
-                ),
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.mail,
-                    color: Color.fromARGB(255, 66, 98, 67),
-                    size: 25,
+              SizedBox(height: 10),
+              SizedBox(
+                height: 50,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextFormField(
+                    initialValue: "example@gmail.com",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 66, 98, 67),
+                      fontSize: 20,
+                    ),
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.mail,
+                        color: Color.fromARGB(255, 66, 98, 67),
+                        size: 25,
+                      ),
+                      isDense: true,
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
                   ),
-                  isDense: true,
-                  filled: true,
-                  fillColor: Colors.white,
                 ),
               ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          OutlinedButton(
-            onPressed: () {},
-            child: const Text(
-              'Sign In',
-              style: TextStyle(fontSize: 24, color: Colors.white),
-            ),
-            style: OutlinedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              side: const BorderSide(
-                width: 1.0,
-                color: Colors.white,
-                style: BorderStyle.solid,
+              const SizedBox(height: 10),
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                style: OutlinedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  side: const BorderSide(
+                    width: 1.0,
+                    color: Colors.white,
+                    style: BorderStyle.solid,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
