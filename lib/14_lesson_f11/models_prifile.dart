56416c6344be741cd6b1842c2d6bbda3e7ffeedb
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-04 16:07:24 +0600

    update

--- lib/14_lesson_f11/models_prifile.dart
+++ lib/14_lesson_f11/models_prifile.dart
@@ -25,3 +25,36 @@ class Student {
     this.marriage,
   });
 }
+
+final mayrambek = Student(
+    id: 1,
+    name: 'Mayrambek',
+    lastName: 'Iskakov',
+    email: 'may@gmail.com',
+    group: 8,
+    phone: '+996502565732',
+    adress: 'Bishkek',
+    marriage: true,
+    jeri: 'Issyk Kol');
+
+final meerim = Student(
+  id: 2,
+  name: 'Meerim',
+  lastName: 'Akmatova',
+  email: 'meerim@gmail.com',
+  group: 8,
+  phone: '+996774856888',
+  jeri: 'Issyk Kol',
+  gender: 1,
+);
+
+final meerim = Student(
+  id: 2,
+  name: 'Meerim',
+  lastName: 'Akmatova',
+  email: 'meerim@gmail.com',
+  group: 8,
+  phone: '+996774856888',
+  jeri: 'Issyk Kol',
+  gender: 1,
+);
