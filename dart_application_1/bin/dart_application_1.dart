8472e273342f984650f3d4a69835573a21c67d4a
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-04 16:27:33 +0600

    update

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,9 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
+final students = <String>[];
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
 }
+
+void controlEmailPhone(String phone, String email) {}
