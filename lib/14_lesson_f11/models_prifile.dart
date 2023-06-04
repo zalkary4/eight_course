a5fc9b7e8111052b3fed30d3406f3cb06e971d8f
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-04 15:50:37 +0600

    update

--- lib/14_lesson_f11/models_prifile.dart
+++ lib/14_lesson_f11/models_prifile.dart
@@ -1,7 +1,12 @@
 class Student {
   final String name;
   final String lastName;
+  final String email;
+  final int? gender;
+  final String? jeri;
   int group;
-  String adress;
-  bool marriage;
+  String phone;
+  int? age;
+  String? adress;
+  bool? marriage;
 }
