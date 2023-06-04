68fa17ef4cfdff9ad811f0b5863500bd5a56cee9
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-06-04 16:38:04 +0600

    update

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,9 +1,29 @@
+// ignore: unused_import
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-final students = <String>[];
+import 'models.dart';
+
+final students = <Student>[
+  mayrambek,
+  meerim,
+  adilbek,
+  altynbek,
+  arsen,
+  bakyt,
+  talant,
+];
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  controlEmailPhone('phone', 'email');
 }
 
-void controlEmailPhone(String phone, String email) {}
+void controlEmailPhone(String phone, String email) {
+  for (final student in students) {
+    // print('student.name');
+    if (phone == student.phone && email == student.email) {
+      print('Welcome');
+    } else {
+      print('Error');
+    }
+  }
+}
